558ce44d63c73f5cfb898191b4f8f231d4525dde
Arthur Emanuel <arthurresende2004@gmail.com>
2024-12-28 18:34:27 -0300

    conceito de componentes

---
+++ Fundamentos do react/src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(post){
+    return(
+        <div>
+            <strong>{post.author}</strong>
+            <p>{post.content}</p>
+        </div>
+    )
+}
--- Fundamentos do react/src/main.jsx
+++ Fundamentos do react/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { App } from './App'
+import { Post } from './Post'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Post 
+        author= "Arthur"
+        content = "pipipipopopo"
+    />
+    <Post
+      author = "teste"
+      content = "swqnhufgwskljdgf"
+    />
   </StrictMode>,
 )
