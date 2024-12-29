41c65f1a42f7d26ff04d811dbca79a161ca253aa
Arthur Emanuel <arthurresende2004@gmail.com>
2024-12-29 17:53:21 -0300

    Finalizando o side bar e perfil do usuário

--- Fundamentos do react/src/App.jsx
+++ Fundamentos do react/src/App.jsx
@@ -1,18 +1,29 @@
-import { Header } from "./components/header"
-import { Post } from "./Post"
+import { Header } from "./components/header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/sidebar";
+
+import styles from './App.module.css';
+import './global.css';
+
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post 
-            author= "Arthur"
-            content = "pipipipopopo"
-        />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
+              author= "Arthur"
+              content = "pipipipopopo"
+          />
         <Post
           author = "teste"
           content = "swqnhufgwskljdgf"
         />
+      </main>
+      </div>
     </div>
   )
 }
---
+++ Fundamentos do react/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine} from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1733837323673-da9b3a98135e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" alt="" 
+            />
+            
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/95095230?v=4" />
+                <strong>Arthur Emanuel</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
